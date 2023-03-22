e6ff91516ce312e02c03d82e7a268a00544fdec3
hsu-z <suumyatnoe2001n@gmail.com>
2023-03-22 21:30:27 +0630

    Sticky Navigation Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,71 @@
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
 
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Scroll Animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const selectEl = document.querySelector(href);
+      selectEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+//More detail btn to download cv btn
+const moreDetailBtn = document.querySelector(".btn--more-detail");
+moreDetailBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const id = moreDetailBtn.getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
